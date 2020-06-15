a6239243d064cdf2901ee9e31a180d0e52155480
samaramiranda <samaramiranda.13@hotmail.com>
2020-06-15 19:08:18 -0300

    Ex0012 - Aula 12

---
+++ Vídeo/scripts/ex012.js
@@ -0,0 +1,12 @@
+var agora = new Date() //pegando a hora exata
+var hora = agora.getHours()
+var minutos = agora.getMinutes()
+console.log(`Agora são ${hora} horas e ${minutos} minutos`)
+
+if (hora < 12){
+  console.log("Bom dia")
+}else if (hora <= 18){
+  console.log("Boa tarde")
+}else {
+  console.log("Boa noite")
+}
