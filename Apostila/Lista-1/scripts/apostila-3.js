1a5fb3270247303264537bfc3ee94a719a508692
samaramiranda <samaramiranda.13@hotmail.com>
2020-06-30 22:01:17 -0300

    Exercício N 3 - Apostila

--- Apostila/Lista-1/scripts/apostila-3.js
+++ Apostila/Lista-1/scripts/apostila-3.js
@@ -1,40 +1,37 @@
-
 function somanotas() {
-  var nota1 = parseFloat(document.getElementById("nota1").value);
-  var nota2 = parseFloat(document.getElementById("nota2").value);
-  var nota3 = parseFloat(document.getElementById("nota3").value);
+    var nota1 = parseFloat(document.getElementById("nota1").value);
+    var nota2 = parseFloat(document.getElementById("nota2").value);
+    var nota3 = parseFloat(document.getElementById("nota3").value);
 
-  var soma = parseFloat(nota1 + nota2 + nota3)
+    var soma = parseFloat(nota1 + nota2 + nota3)
 
-  document.getElementById("soma")
-    .setAttribute("value", soma)
+    document.getElementById("soma")
+        .setAttribute("value", soma)
 }
 
 
 function medianotas() {
-  var nota1 = parseFloat(document.getElementById("nota1").value)
-  var nota2 = parseFloat(document.getElementById("nota2").value)
-  var nota3 = parseFloat(document.getElementById("nota3").value)
+    var nota1 = parseFloat(document.getElementById("nota1").value)
+    var nota2 = parseFloat(document.getElementById("nota2").value)
+    var nota3 = parseFloat(document.getElementById("nota3").value)
 
-  var media = parseFloat((nota1 + nota2 + nota3) / 3)
+    var media = parseFloat((nota1 + nota2 + nota3) / 3)
 
-  document.getElementById("media")
-    .setAttribute("value", media)
+    document.getElementById("media")
+        .setAttribute("value", media)
 }
 
 function situacaoaluno() {
-  var nota1 = parseFloat(document.getElementById("nota1").value)
-  var nota2 = parseFloat(document.getElementById("nota2").value)
-  var nota3 = parseFloat(document.getElementById("nota3").value)
+    var nota1 = parseFloat(document.getElementById("nota1").value)
+    var nota2 = parseFloat(document.getElementById("nota2").value)
+    var nota3 = parseFloat(document.getElementById("nota3").value)
 
-  var soma = parseFloat(nota1 + nota2 + nota3)
+    var soma = parseFloat(nota1 + nota2 + nota3)
 
-  soma > 50 ?
-    document.getElementById("situacao")
-      .setAttribute("value", "Aprovado :D")
-  : document.getElementById("situacao")
-      .setAttribute("value", "Reprovado :(")
-  
+    soma > 50 ?
+        document.getElementById("situacao")
+        .setAttribute("value", "Aprovado :D") :
+        document.getElementById("situacao")
+        .setAttribute("value", "Reprovado :(")
+
 }
-
-
