0f5d373ca4d564cd3d2fb999b4225dbd2c6672ea
samaramiranda <samaramiranda.13@hotmail.com>
2020-07-02 18:22:26 -0300

    Exercício - Aula 16 Funções

--- Vídeo/Exercicios/analisador-números/analisador.js
+++ Vídeo/Exercicios/analisador-números/analisador.js
@@ -1,3 +1,49 @@
-function adicionar(){
-  
+var valores = []
+var select = document.getElementById("selectresul")
+
+function adicionar() {
+  let numero = Number(document.getElementById("num").value)
+
+  if (numero < 1 || numero > 100) {
+    alert("Digite um número entre 1 e 100!")
+  } else {
+    if (valores.indexOf(numero) != -1) {
+      alert("Número informado já existente na lista!")
+    } else {
+      valores.push(numero)
+      let item = document.createElement("option")
+      item.text = `Valor ${numero} adicionado.`
+      select.appendChild(item)
+    }
+  }
+  console.log(valores)
+}
+
+function finalizar() {
+  let resul = document.getElementById("resultado")
+
+  if (select.length == "") {
+    alert("Lista vazia! Adicione um número.")
+  } else {
+    let somaresul = 0;
+
+    function soma() {
+      for (let pos in valores) {
+        somaresul += valores[pos]
+      }
+      return somaresul
+    }
+
+    let totalsoma = soma(valores)
+    let media = totalsoma / valores.length
+
+    valores.sort()
+
+    resul.innerHTML = `Ao todo temos ${valores.length} números cadastrados. <br>
+      O maior valor informado foi ${valores[valores.length - 1]}. <br>
+      O menor valor informado foi ${valores[0]}. <br>
+      Somando todos os valores, temos ${totalsoma}. <br>
+      A média dos valores digitados é ${media}.`
+  }
+
 }
