3b625b36c07b10bde2517070037f9148f15d2845
samaramiranda <samaramiranda.13@hotmail.com>
2020-07-06 18:02:01 -0300

    Aula 3 - Alertas, Caixas de Diálogos e Operadores

---
+++ Curso-vanilla-javascript/aula-3/aula-3.js
@@ -0,0 +1,12 @@
+//ALERTAS, CAIXAS DE DIÁLOGOS E OPERADORES
+
+//alert
+alert("Esse é um alerta")
+
+//caixa de dialog
+confirm("Deseja apagar o arquivo? \nEle será excluído!")
+
+//caixa de prompt
+prompt("Qual sua idade?", "dd/mm/yyyy")
+
+
