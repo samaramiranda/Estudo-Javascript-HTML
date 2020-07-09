3a1a3bdefb223bf2ecc4cbf150f55336f3db9ebc
samaramiranda <samaramiranda.13@hotmail.com>
2020-07-09 18:48:49 -0300

    Aula 8 - Objetos Personalizados II

---
+++ Curso-vanilla-javascript/aula-8/aula-8.js
@@ -0,0 +1,28 @@
+let person = {
+  name: "Samara",
+  age: 20,
+  cc: 123
+}
+
+let operation = {
+  sacar: function (saldo, valor) { //método
+    return saldo -= valor
+  },
+  depositar: function (saldo, valor) {
+    return saldo += valor
+  }
+}
+
+let bank = {
+  person: person,
+  operation: operation,
+  saldo: 50
+}
+
+let value = 40;
+
+//Saque
+console.log(`${bank.person.name} portador da conta ${bank.person.cc} realizou um saque no valor de ${value} ficando com ${bank.operation.sacar(bank.saldo, value)}`)
+
+//Depósito
+console.log(`${bank.person.name} portador da conta ${bank.person.cc} realizou um depósito no valor de ${value} ficando com ${bank.operation.depositar(bank.saldo, value)}`)
