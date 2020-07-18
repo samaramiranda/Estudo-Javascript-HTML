731b355a1be01b9a21409b4f01198ee9b613f0cf
samaramiranda <samaramiranda.13@hotmail.com>
2020-07-17 21:06:07 -0300

    Métodos de Objeto e JSON

---
+++ Curso-javascript-ninja/Exemplos/aula-12-070.js
@@ -0,0 +1,35 @@
+//MÉTODOS DE OBJETO
+var obj = {
+  x: 1,
+  y: 2
+}
+var obj2 = Object.create(obj)
+obj2.x = 10
+var obj3 = Object.create(obj2)
+
+
+//OBJECT.KEYS
+//Retorna todas as propriedades do objeto
+console.log(Object.keys(obj))
+
+//para contar a quantidade de propriedades de um objeto
+Object.keys(obj).length
+
+
+//ISPROTOTYPEOF
+//Verifica se o objeto é protótipo de outro (outro herdou props dele)
+obj.isPrototypeOf(obj2) //"obj2" é herdado de "obj"? True
+obj.isPrototypeOf(obj3)//"obj" é protótipo do "obj2" e "obj3"
+
+
+//JSON.STRINGIFY(OBJ)
+//transforma o objeto em uma string (formato JSON)
+JSON.stringify(obj)
+var objString = JSON.stringify(obj)
+console.log(objString)
+
+
+//JSON.PARSE(OBJ)
+//transforma a string em um objeto novamente
+JSON.parse(objString)
+console.log(JSON.parse(objString))
