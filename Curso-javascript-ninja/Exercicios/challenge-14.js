7a443acb4d5a08ca959c0f09ceefe6b2e5c89dd3
samaramiranda <samaramiranda.13@hotmail.com>
2020-07-19 15:38:15 -0300

    Desafio 14 - Métodos de arrays II

--- Curso-javascript-ninja/Exercicios/challenge-14.js
+++ Curso-javascript-ninja/Exercicios/challenge-14.js
@@ -51,7 +51,86 @@ Envolva todo o código desse desafio em uma IIFE.
   O cálculo deve começar com zero.
   Mostre o resultado no console.
   */
-  console.log('\nOperation:');
+  let operation = justMod2Or3.reduce(function (acumulado, atual) {
+    return (acumulado + 1) * atual
+  }, 0)
+  console.log('\nOperation:')
+  console.log(operation)
 
+  /*
+  Faça o mesmo cálculo passado acima, mas começando do último item para o
+  primeiro. O nome da variável deve ser operation2. Mostre o resultado no
+  console.
+  */
+  let operation2 = justMod2Or3.reduceRight(function (acumulado, atual) {
+    return (acumulado + 1) * atual
+  })
+  console.log('\nOperation 2:')
+  console.log(operation2)
 
+  /*
+  Crie um array chamado `name`. Cada elemento desse array deve ser uma sílaba
+  do seu nome. Vamos reduzir esse array, juntando todas as sílabas, mas usando
+  a "língua do P".
+  PS.: Lembra da língua do "P"? Não? A língua do "P" é uma brincadeira
+  infantil, onde você coloca a letra "P" antes de cada sílaba de uma palavra
+  falada, como se você estivesse falando em código xD
+  */
+  let name = ["sa", "ma", "ra"]
+  let reduceP = name.reduce(function (acumulado, atual) {
+    return acumulado + "P" + atual
+  }, "")
+  console.log('\nSeu nome na língua do "P":')
+  console.log(reduceP)
+
+  /*
+  Crie uma variável chamada `inversedName`, que reduzirá o array em uma string
+  e atribuirá o seu nome invertido (usando o array criado acima).
+  */
+  let inversedName = name.reduceRight(function (acumulado, atual) {
+    return acumulado + atual
+  })
+  console.log('\nInversed Name:')
+  console.log(inversedName)
+
+  //Mostre no console o array `numberObjects`.
+  console.log('\nNumber objects');
+  console.log(numberObjects)
+
+  /*
+  Verifique se existem em algum índice de numberObjects um objeto ìgual a
+  { number: 2 }. Se houver, mostre no console:
+  - "Existe um objeto { number: 2 } em numberObjects!"
+  Senão, mostre a frase:
+  - "Não existe um objeto { number: 2 } em numberObjects :("
+  Consegue prever o resultado? Deixe uma mensagem no console tentando explicar
+  o que acontece ;)
+  */
+  console.log('\nExiste um { number: 2 } em numberObjects?');
+  let obj2 = numberObjects[1] //recebe a referência de memória do numberObjects[1]
+  if (numberObjects.indexOf(obj2) > -1) {
+    console.log("Existe um objeto { number: 2 } em numberObjects!")
+  } else {
+    console.log("Não existe um objeto { number: 2 } em numberObjects :(")
+  }
+
+  /*
+  Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
+  será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
+  */
+  console.log('\nE buscando a partir do último índice, o { number: 2 } existe?');
+  if (numberObjects.lastIndexOf(obj2) > -1) {
+    console.log("Existe um objeto { number: 2 } em numberObjects!")
+  } else {
+    console.log("Não existe um objeto { number: 2 } em numberObjects :(")
+  }
+
+  /*
+  Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
+  formato de String.
+  */
+  console.log('\njustMod2Or3 é um array? Se for, a representação dele em String é:');
+  if (Array.isArray(justMod2Or3)) {
+    console.log(justMod2Or3.toString())
+  }
 })()
