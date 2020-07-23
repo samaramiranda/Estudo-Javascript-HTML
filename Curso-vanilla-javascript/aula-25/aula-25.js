2aace8c6c71ad595dae4a62ed011811065451726
samaramiranda <samaramiranda.13@hotmail.com>
2020-07-23 20:14:38 -0300

    Aula 25 - Formulários I

---
+++ Curso-vanilla-javascript/aula-25/aula-25.js
@@ -0,0 +1,43 @@
+//FORMULÁRIOS I
+
+(function (window, document) {
+  "use strict"
+
+  let form1 = document.querySelector("#form1")
+  let inpText = document.querySelector("#inpText")
+  let txtArea = document.querySelector("#txtArea")
+  let lengthArea = document.querySelector("#lengthArea")
+
+  form1.action = "teste.php"
+  inpText.style.backgroundColor = "gray"
+
+
+  //VALIDANDO INPUT VAZIO
+  // if(inpText.value === ""){
+  //   alert("Preencha o campo")
+  // }
+  //OU
+  function validateInput(event){
+    if(event.target.value == ""){
+      return alert("Preencha o campo")
+    }
+  }
+  inpText.addEventListener("blur", validateInput)
+
+
+  //TEXT AREA
+  //Validando textArea com a mesma função do input
+  txtArea.addEventListener("blur", validateInput)
+
+  //Contando a quantidade de caracteres digitados
+  function countArea(event){
+    lengthArea.innerHTML = event.target.textLength
+
+    //se chegar a 30 caracteres o textArea é desabilitado
+    if(event.target.textLength >= 30){
+      txtArea.setAttribute("disabled", "disabled")
+    }
+  }
+  txtArea.addEventListener("keydown", countArea)
+
+})(window, document)
