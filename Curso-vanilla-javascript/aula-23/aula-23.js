b06b6e67692d4a8783cee5784578744c9433cccd
samaramiranda <samaramiranda.13@hotmail.com>
2020-07-23 19:17:38 -0300

    Aula 23 - Expressões Regulares I

--- Curso-vanilla-javascript/aula-23/aula-23.js
+++ Curso-vanilla-javascript/aula-23/aula-23.js
@@ -1,7 +1,6 @@
 //EXPRESSÕES REGULARES I
 
-(function(window, document){
-  "use strict"
+(function(){
 
   let msg = "A História do Brasil é dividida\n, consensualmente e para fins didáticos, em três períodos principais: Período Colonial, Período Imperial e Período Republicano. Entretanto, tais divisões existem apenas para organizar esquematicamente os principais conteúdos sobre a formação do Brasil, tendo como ponto de partida o ano do descobrimento, isto é, 1500. Entretanto, é sabido que, no território em que se “formou o Brasil”, havia, antes, várias tribos nativas com aspectos culturais muito particulares. Mesmo antes da formação dessas tribos, houve também povos primitivos que deixaram os vestígios de sua cultura em vários lugares do território brasileiro (Veja Pré-história brasileira) há milhares de anos. A esse período da História do Brasil cujo tema central é o estudo dos povos nativos, isto é, dos povos indígenas, dá-se o nome de Período Pré-Cabralino. Essa nomenclatura faz referência a Pedro Álvares Cabral, cuja chegada em terras brasileiras é considerada o marco inaugural da História do Brasil. A partir de então, de 1500 em diante, sobretudo a partir da década de 1530, teve início a fase do Brasil Colônia."
   console.log(msg)
@@ -32,4 +31,4 @@
   let result = msg.match(regex)
   console.log(result)
   
-})(window, document)
+})()
