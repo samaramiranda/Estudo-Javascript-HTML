0c5f7327008139620bcaf52a765913d8d87dbc98
samaramiranda <samaramiranda.13@hotmail.com>
2020-08-07 16:41:50 -0300

    Criando função Debounce do Zero com JavaScript

--- Conteudo-geral/JS/Debounce/scripts.js
+++ Conteudo-geral/JS/Debounce/scripts.js
@@ -1,6 +1,25 @@
-function handleKeyUp(event){
-  console.log(event.target.value)
+const filterUsers = async (name) => {
+  return fetch(`https://jsonplaceholder.typicode.com/users?name_like=${name}`)
+    .then(res => res.json())
 }
 
+function debounceEvent(fn, wait = 1000, time) {
 
-document.querySelector("input").addEventListener("keyup", handleKeyUp)
+  //função executada sempre que clica numa tecla
+  return function () {
+    clearTimeout(time)
+
+    time = setTimeout(() => {
+      //função de "HandleKeyUp" é executada utilizando o "event" passado por argumento
+      fn.apply(this, arguments) 
+    }, wait)
+  }
+}
+
+function handleKeyUp(event) {
+    filterUsers(event.target.value)
+    .then(users => console.log(users.map(user => user.name)))
+  }
+
+//debounceEvent é executa quando abre a página, então ela se recebe o seu próprio retorno e se torna a função interna
+document.querySelector("input").addEventListener("keyup", debounceEvent(handleKeyUp))
