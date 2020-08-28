1449d3a138cd1eaa854f4291eadb4666c707a744
samaramiranda <samaramiranda.13@hotmail.com>
2020-08-28 19:48:39 -0300

    Calculadora de IMC em React Native

--- Conteudo-geral/React-Native/Calculadora_IMC/App.js
+++ Conteudo-geral/React-Native/Calculadora_IMC/App.js
@@ -5,15 +5,15 @@ export default function App() {
   const [peso, setPeso] = useState("");
   const [altura, setAltura] = useState("");
 
-  function handleSubmit(){
+  function handleSubmit() {
     const alt = altura / 100
     const imc = peso / (alt * alt)
-    
-    if(imc < 18.6){
+
+    if (imc < 18.6) {
       alert("Você está abaixo do peso! " + imc.toFixed(2))
-    }else if(imc >= 18.6 && imc < 24.9){
+    } else if (imc >= 18.6 && imc < 24.9) {
       alert("Peso ideal! " + imc.toFixed(2))
-    }else if (imc >= 24.9 < 34.9){
+    } else if (imc >= 24.9 < 34.9) {
       alert("Levemente acima do peso! " + imc.toFixed(2))
     }
   }
