8db7fb8abc5db4f7ace2d48073c5137285e68fec
samaramiranda <samaramiranda.13@hotmail.com>
2020-10-21 22:32:04 -0300

    Aula 043 - Introdução a React Hooks

--- Conteudo-geral/ReactJS/Curso-reactJS-basico/app2/src/App.js
+++ Conteudo-geral/ReactJS/Curso-reactJS-basico/app2/src/App.js
@@ -1,34 +1,20 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 
-import Navegacao from "./components/Navegacao"
-import Home from "./components/Home"
-import Servicos from "./components/Servicos"
-import Contatos from "./components/Contatos"
+const App = () => {
+  const [nome, setNome] = useState("Samara")
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Navegacao></Navegacao>
-
-          <Switch>
-            <Route exact path="/">
-              <Home></Home>
-            </Route>
-            <Route path="/servicos">
-              <Servicos></Servicos>
-            </Route>
-            <Route path="/contatos">
-              <Contatos></Contatos>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    )
+  const alterarNome = () => {
+    setNome("Miranda")
   }
+
+  return (
+    <div>
+      <p>
+        O nome é: {nome}
+      </p>
+      <button onClick={alterarNome}>Alterar</button>
+    </div>
+  )
 }
 
 export default App
