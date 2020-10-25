4625e004a2175bbb45e22d11b41801fb97f1f040
samaramiranda <samaramiranda.13@hotmail.com>
2020-10-25 20:49:36 -0300

    Adicionando função

--- Conteudo-geral/ReactJS/Curso-reactJS-basico/app2/src/App.js
+++ Conteudo-geral/ReactJS/Curso-reactJS-basico/app2/src/App.js
@@ -2,9 +2,15 @@ import React, { useState } from "react"
 
 const App = () => {
   const [nome, setNome] = useState("Samara")
+  const [valor, setValor] = useState(1)
 
   const alterarNome = () => {
     setNome("Miranda")
+  }
+
+  const alterarValor = () => {
+    const novoValor = valor * 2
+    setValor(novoValor)
   }
 
   return (
@@ -12,7 +18,11 @@ const App = () => {
       <p>
         O nome é: {nome}
       </p>
+      <p>
+        O dobro é: {valor}
+      </p>
       <button onClick={alterarNome}>Alterar</button>
+      <button onClick={alterarValor}>Multiplicar</button>
     </div>
   )
 }
