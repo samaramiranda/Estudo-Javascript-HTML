6010796342770d1cd54e773b861786a1827d5615
samaramiranda <samaramiranda.13@hotmail.com>
2020-10-28 22:48:48 -0300

    Aula 043 - Introdução ao React Hooks

--- Conteudo-geral/ReactJS/Curso-reactJS-basico/app2/src/App.js
+++ Conteudo-geral/ReactJS/Curso-reactJS-basico/app2/src/App.js
@@ -2,21 +2,9 @@ import React, { useState } from "react"
 
 const App = () => {
   const [nome, setNome] = useState("Samara")
-  const [valor, setValor] = useState(1)
-  const [soma, setSoma] = useState(0)
 
   const alterarNome = () => {
     setNome("Miranda")
-  }
-
-  const alterarValor = () => {
-    const novoValor = valor * 2
-    setValor(novoValor)
-  }
-
-  const somar = () => {
-    const resulSoma = soma + 2
-    setSoma(resulSoma)
   }
 
   return (
@@ -24,15 +12,7 @@ const App = () => {
       <p>
         O nome é: {nome}
       </p>
-      <p>
-        O dobro é: {valor}
-      </p>
-      <p>
-        O resultado da soma é: {soma}
-      </p>
       <button onClick={alterarNome}>Alterar</button>
-      <button onClick={alterarValor}>Multiplicar</button>
-      <button onClick={somar}>Somar + 2</button>
     </div>
   )
 }
